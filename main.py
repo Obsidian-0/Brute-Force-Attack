762bcb1f264793e88b020ab7c5b0e980e737c8a6
devil-458 <affanchohan786@gmail.com>
2026-05-16 00:43:41 +0500

    Fix indentation in login function

--- main.py
+++ main.py
@@ -35,36 +35,35 @@ def registration():
         conn.close()
 
 def login():
-   user = input("Enter username: ")
-   password = input("Enter password: ")
-   conn = sqlite3.connect('brute_force.db')
-   cursor = conn.cursor()
-   cursor.execute('Select salt,password_hash,failed_attempts,is_locked from users where username = ? ',(user,))
-   result = cursor.fetchone()
-   if result is None:
-       print("User no found.")
-       conn.close()
-       return
-   salt, password_hash, failed_attempts, is_locked = result
-   if is_locked:
-         print("Account is locked due to too many failed login attempts.")
-         conn.close()
-         return
-   salt_bytes = bytes.fromhex(salt)
-   computed_hash = hashlib.sha256(password.encode()+ salt_bytes).hexdigest()
-   if computed_hash == password_hash:
-       
-       print("Login Sexcessfull")
-       cursor.execute('Update users set failed_attempts = 0 where username =?',(user,))
-   else:
-       failed_attempts +=1
-       print(f"Wrong Password! Attempts : {failed_attempts}/5")
-       cursor.execute('update users set failed_attempts =? where username = ?',(failed_attempts,user))
-       if failed_attempts>=5:
-           cursor.execute('Update users set is_locked =1 where username = ?',(user,))
-           print('Account Locked!')
-   conn.commit()
-   conn.close()
+    user = input("Enter username: ")
+    password = input("Enter password: ")
+    conn = sqlite3.connect('brute_force.db')
+    cursor = conn.cursor()
+    cursor.execute('Select salt,password_hash,failed_attempts,is_locked from users where username = ? ',(user,))
+    result = cursor.fetchone()
+    if result is None:
+        print("User no found.")
+        conn.close()
+        return
+    salt, password_hash, failed_attempts, is_locked = result
+    if is_locked:
+        print("Account is locked due to too many failed login attempts.")
+        conn.close()
+        return
+    salt_bytes = bytes.fromhex(salt)
+    computed_hash = hashlib.sha256(password.encode()+ salt_bytes).hexdigest()
+    if computed_hash == password_hash:
+        print("Login Sexcessfull")
+        cursor.execute('Update users set failed_attempts = 0 where username =?',(user,))
+    else:
+        failed_attempts +=1
+        print(f"Wrong Password! Attempts : {failed_attempts}/5")
+        cursor.execute('update users set failed_attempts =? where username = ?',(failed_attempts,user))
+        if failed_attempts>=5:
+            cursor.execute('Update users set is_locked =1 where username = ?',(user,))
+            print('Account Locked!')
+    conn.commit()
+    conn.close()
 
 if __name__ == "__main__":
     create_database()
